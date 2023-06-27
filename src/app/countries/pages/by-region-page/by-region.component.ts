16a3fc10a404a8c0e6ff370dc6915640c87c9b8d
Jeremy <ing_fischer@outlook.com>
2023-06-26 21:10:43 -0300

    Fin de la sección

---
+++ src/app/countries/pages/by-region-page/by-region.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { CountriesService } from '../../services/countries.service';
+
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'app-by-region',
+  templateUrl: './by-region.component.html',
+  styles: [
+  ]
+})
+export class ByRegionPageComponent {
+
+  public countries: Country[] = [];
+
+  constructor(private countriesService: CountriesService) { }
+
+  searchByRegion(region: string): void {
+    this.countriesService.searchRegion(region)
+      .subscribe(countries => {
+        this.countries = countries;
+      });
+  }
+
+}
